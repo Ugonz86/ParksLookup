2bf08db1cbd878b5811b9a0584548526ca0aa5d1
Ugonz86 <ugonzalez86@gmail.com>
2020-01-31 10:38:31 -0800

    Initial commit. Basic backend structure created with dotnet-new-webapi. Database setup added

---
+++ Models/Park.cs
@@ -0,0 +1,10 @@
+namespace ParksLookup.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public string Alerts { get; set; }
+    }
+}
